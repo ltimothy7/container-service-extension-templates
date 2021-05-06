bb436a7b168c153ca69f4b19dc2da94277497d41
ltimothy <ltimothy@vmware.com>
2021-05-06 15:44:58 -0400

    starting tkgm template
    Signed-off-by: ltimothy <ltimothy@vmware.com>

---
+++ scripts/ubuntu-20.04_k8-1.20_antrea-0.13_rev1/mstr.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+while [ `systemctl is-active docker` != 'active' ]; do echo 'waiting for docker'; sleep 5; done
+kubeadm init --pod-network-cidr=100.66.0.0/16 --service-cidr=100.77.0.0/16 --kubernetes-version=v1.20.6 > /root/kubeadm-init.out
+mkdir -p /root/.kube
+cp -f /etc/kubernetes/admin.conf /root/.kube/config
+chown $(id -u):$(id -g) /root/.kube/config
+
+export kubever=$(kubectl version --client | base64 | tr -d '\n')
+kubectl apply -f /root/antrea_0.11.0.yaml
+systemctl restart kubelet
+while [ `systemctl is-active kubelet` != 'active' ]; do echo 'waiting for kubelet'; sleep 5; done
