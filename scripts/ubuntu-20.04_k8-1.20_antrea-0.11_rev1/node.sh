7059fafbc4eed3657af80e3d00e8cf7c632197ad
ltimothy <ltimothy@vmware.com>
2021-05-13 13:29:50 -0400

    removing node commented line
    Signed-off-by: ltimothy <ltimothy@vmware.com>

--- scripts/ubuntu-20.04_k8-1.20_antrea-0.11_rev1/node.sh
+++ scripts/ubuntu-20.04_k8-1.20_antrea-0.11_rev1/node.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -e
 while [ `systemctl is-active docker` != 'active' ]; do echo 'waiting for docker'; sleep 5; done
-#kubeadm join --token {token} {ip}:6443 --discovery-token-unsafe-skip-ca-verification
 kubeadm join {ip_port} --token {token} --discovery-token-ca-cert-hash {discovery_token_ca_cert_hash}
