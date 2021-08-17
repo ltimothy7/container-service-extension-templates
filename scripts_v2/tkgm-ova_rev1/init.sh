861f40eb8cefd2d60f60443df2c05431a250a855
ltimothy <ltimothy@vmware.com>
2021-08-17 11:30:38 -0400

    fix for networking
    Signed-off-by: ltimothy <ltimothy@vmware.com>

--- scripts_v2/tkgm-ova_rev1/init.sh
+++ scripts_v2/tkgm-ova_rev1/init.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+mv /etc/netplan/01-netcfg.yaml /etc/netplan/01-netcfg.yaml.bak
+mv /etc/netplan/01-netcfg.yaml.BeforeVMwareCustomization /etc/netplan/01-netcfg.yaml.BeforeVMwareCustomization.bak
+mv /etc/netplan/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml.bak
+mv /etc/netplan/50-cloud-init.yaml.BeforeVMwareCustomization /etc/netplan/50-cloud-init.yaml.BeforeVMwareCustomization.bak
+
 sed -i -e 's/^PasswordAuthentication yes/PasswordAuthentication no/' -e 's/^PermitRootLogin yes/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config
 
 apt remove -y cloud-init
