01a08a493d9d491244b9c33d1f2e784165edec33
ltimothy <ltimothy@vmware.com>
2021-08-18 19:08:56 -0400

    conditional renaming
    Signed-off-by: ltimothy <ltimothy@vmware.com>

--- scripts_v2/tkgm-ova_rev1/init.sh
+++ scripts_v2/tkgm-ova_rev1/init.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-mv /etc/netplan/01-netcfg.yaml /etc/netplan/01-netcfg.yaml.bak
-mv /etc/netplan/01-netcfg.yaml.BeforeVMwareCustomization /etc/netplan/01-netcfg.yaml.BeforeVMwareCustomization.bak
-mv /etc/netplan/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml.bak
-mv /etc/netplan/50-cloud-init.yaml.BeforeVMwareCustomization /etc/netplan/50-cloud-init.yaml.BeforeVMwareCustomization.bak
+netcfg1_path=/etc/netplan/01-netcfg.yaml
+netcfg1_before_path=/etc/netplan/01-netcfg.yaml.BeforeVMwareCustomization
+netcfg50_path=/etc/netplan/50-cloud-init.yaml
+netcfg50_before_path=/etc/netplan/50-cloud-init.yaml.BeforeVMwareCustomization
+
+[ -f $netcfg1_path ] && mv $netcfg1_path /etc/netplan/01-netcfg.yaml.bak
+[ -f $netcfg1_before_path ] && mv $netcfg1_before_path /etc/netplan/01-netcfg.yaml.BeforeVMwareCustomization.bak
+[ -f $netcfg50_path ] && mv $netcfg50_path /etc/netplan/50-cloud-init.yaml.bak
+[ -f $netcfg50_before_path ] && mv $netcfg50_before_path /etc/netplan/50-cloud-init.yaml.BeforeVMwareCustomization.bak
 
 sed -i -e 's/^PasswordAuthentication yes/PasswordAuthentication no/' -e 's/^PermitRootLogin yes/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config
 
@@ -12,3 +17,5 @@ apt remove -y cloud-init
 dpkg-reconfigure openssh-server
 sync
 sync
+
+exit 0
